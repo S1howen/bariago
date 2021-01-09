31ddeb301d08d26f89f44693adf5be9b90e82f70
asgeir <asgeir.bar@gmail.com>
2021-01-09 19:22:17 +0000

    Adding movement test

--- src/move_x.py
+++ src/move_x.py
@@ -12,6 +12,7 @@ import math
 import rospy
 from geometry_msgs.msg import Twist
 
+
 class Velocity(object):
 
     def __init__(self, min_velocity, max_velocity, num_steps):
@@ -38,18 +39,35 @@ class Velocity(object):
         return value * max_value
 
 
-class ForeverSpin():
-    def __init__(self, spinTime):
+class Move_from_x_to_y():
+    def __init__(self, asked_movement):
         self._pub_cmd = rospy.Publisher('key_vel', Twist)
-
         self._hz = rospy.get_param('~hz', 10)
-        self.runtime = spinTime*self._hz
+        self.rotation_rate = 0
+        if asked_movement == "right":
+            self.moveTime = 5.3
+            self._forward_rate = rospy.get_param('~forward_rate', 0.0)
+            self._rotation_rate = rospy.get_param('~rotation_rate', 0.3)
+        elif asked_movement == "left":
+            self.moveTime = 5.3
+            self._forward_rate = rospy.get_param('~forward_rate', 0.0)
+            self._rotation_rate = rospy.get_param('~rotation_rate', 0.3)
+        elif asked_movement == "start_rum":
+            self.steps = 1
+            self._forward_rate = rospy.get_param('~forward_rate', 0.3)
+            self._rotation_rate = rospy.get_param('~rotation_rate', 0.0)
+            self.moveTime = 1.6666*self.steps
+        elif asked_movement == "rum_cola":
+            self.steps = 2
+            self._forward_rate = rospy.get_param('~forward_rate', 0.3)
+            self._rotation_rate = rospy.get_param('~rotation_rate', 0.0)
+            self.moveTime = 1.6666*self.steps
+        self.runtime = moveTime * self._hz
 
-        self._forward_rate = rospy.get_param('~forward_rate', 0.8)
-        self._backward_rate = rospy.get_param('~backward_rate', 0.5)
-        self._rotation_rate = rospy.get_param('~rotation_rate', 1.6)
+        #self._backward_rate = rospy.get_param('~backward_rate', 0.3)
         self._angular = 0
         self._linear = 0
+
 
     def run(self):
         rate = rospy.Rate(self._hz)
@@ -67,7 +85,7 @@ class ForeverSpin():
         return twist
 
     def _set_velocity(self):
-        linear = 0.0
+        linear = 1.0
         angular = 1.0
         if linear > 0:
             linear = linear * self._forward_rate
@@ -82,10 +100,10 @@ class ForeverSpin():
         self._pub_cmd.publish(twist)
 
 
-
 def main():
+    asked_movement = sys.argv
     rospy.init_node('spin')
-    app = ForeverSpin(10)
+    app = Move_from_x_to_y(asked_movement)
     app.run()
 
 
